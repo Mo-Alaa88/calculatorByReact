febc434f0dc5ada8eecc5a3edb68318cf1e3bce8
Yarab <judymotors@gmail.com>
2019-08-15 03:16:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {connect} from 'react-redux' ;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+class App extends Component{
+  // state ={
+  //   count:0
+  // }
+
+  increase = ()=>{
+    this.setState({
+      count:this.state.count + 1
+    })
+  }
+
+  decrease = ()=>{
+    this.setState({
+      count:this.state.count - 1
+    })
+  }
+  render(){
+    console.log(this.props);
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Install Redux
+          </p>
+    
+        </header>
+        <section>
+          <button onClick={this.increase}>+</button>
+          <p>{this.props.count}</p>
+          <button onClick={this.decrease}>-</button>
+
+        </section>
+      </div>
+    );
+  }
 }
-
-export default App;
+function mapStateToProps(state){
+  return{
+    count : state.count
+  }
+}
+  export default connect(mapStateToProps)(App);
